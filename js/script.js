ed8514f77753f51f1fa0984628d5755300cf2763
mangchubet <haikalfiransyah28@gmail.com>
2025-10-28 12:55:51 +0900

    menambahkan content di page personal, dan mengedit beberapa hal lainnya

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ const stickyBoxLeft = document.getElementById('sticky-content-left');
   // efek scroll
 window.addEventListener("scroll", () => {
   const scrollPos = window.scrollY;
-  const windowHeight = window.innerHeight;
+  const windowHeight = window.innerHeight - 50;
   const totalHeight = document.body.scrollHeight - windowHeight;
 
   if (scrollPos > 150) {
@@ -159,104 +159,3 @@ if (btnSidebar.length && tabs.length) {
     });
   });
 }
-
-
-// function allReady() {
-//   return sections.every((id) => loaded.has(id));
-// }
-
-// function setupFeatures() {
-
-
-
-
-//   console.log("✅ Semua fitur sudah siap!");
-// }
-
-// // dengarkan event loadHTML
-// document.addEventListener("htmlLoaded", (e) => {
-//   loaded.add(e.detail);
-//   console.log("Loaded:", e.detail);
-
-//   if (allReady()) {
-//     setupFeatures();
-//   }
-// });
-
-
-
-
-
-// import { cubicBezier, smoothScroll } from "./script_utils.js";
-
-// let toggleTheme; // biar bisa dipakai di home
-// // Definisi easing
-//   const ease = cubicBezier(0, 0.25, 0.25, 1);
-//   const easeIn = cubicBezier(0, 0.42, 1, 1);
-//   const easeOut = cubicBezier(0, 0, 0.58, 1);
-//   const easeInOut = cubicBezier(0, 0.42, 0.58, 1);
-//   const custom = cubicBezier(0.68, -0.55, 0.27, 1.55);
-
-// document.addEventListener("htmlLoaded", (e) => {
-//   if (e.detail === "header") {
-//     const mainHeader = document.getElementById("main-header");
-
-//     // Efek scroll
-//     window.addEventListener("scroll", () => {
-//       if(window.scrollY > 50){
-//         mainHeader.classList.add("scrolled");
-//       }else{
-//         mainHeader.classList.remove("scrolled");
-//       }
-//     });
-
-  
-//     // Nav active + smooth scroll
-//     const navLinks = mainHeader.querySelectorAll("a[href^='#']");
-//     navLinks.forEach((link) => {
-//       link.addEventListener("click", (e) => {
-//         e.preventDefault();
-//         navLinks.forEach((a) => a.classList.remove("active"));
-//         link.classList.add("active");
-
-//         const target = document.querySelector(link.getAttribute("href"));
-//         if (target) smoothScroll(target, 800, custom);
-//       });
-//     });
-
-//     // Simpan toggleTheme
-//     toggleTheme = document.getElementById("toggleTheme");
-//   }
-
-//   if (e.detail === "home" && toggleTheme) {
-//     const home = document.querySelector(".home");
-//     toggleTheme.addEventListener("change", (event) => {
-//       document.body.classList.toggle("light-theme", event.target.checked);
-//       home.classList.toggle("active");
-//     });
-//   }
-
-//   if (e.detail === "experient") {
-//     const btnSidebar = document.querySelectorAll(".sidebar ul li");
-//     const tabs = document.querySelectorAll(".tab");
-
-//     btnSidebar.forEach((btn) => {
-//       btn.addEventListener("click", () => {
-//         btnSidebar.forEach((b) => b.classList.remove("active"));
-//         tabs.forEach((tab) => {
-//           tab.classList.remove("active");
-//           tab.style.display = "none";
-//         });
-
-//         btn.classList.add("active");
-
-//         const target = document.getElementById(btn.dataset.tab);
-//         target.style.display = "block";
-
-//         setTimeout(() => {
-//           target.classList.add("active");
-//         }, 10);
-//       });
-//     });
-//   }
-// });
